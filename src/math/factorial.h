3c705ae67a1bc6d68dd6d476e408920e3c5aeb3e
Ales Nosek <ales.nosek@gmail.com>
2015-06-22 22:19:09 -0700

    Prefer /** comment style over /*!

--- src/math/factorial.h
+++ src/math/factorial.h
@@ -1,11 +1,11 @@
 #ifndef MYPROJECT_MATH_FACTORIAL_H
 #define MYPROJECT_MATH_FACTORIAL_H
 
-/*! @ingroup group_math */
+/** @ingroup group_math */
 namespace myproject {
     namespace math {
 
-        /*! @ingroup group_math */
+        /** @ingroup group_math */
         int fact(int n);
     }
 }
